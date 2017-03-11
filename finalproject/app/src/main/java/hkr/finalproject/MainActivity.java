ae2fce810586ecf89e755574d909ea2490b7f1a2
harrikr <kurniawanharry165@gmail.com>
2017-03-11 09:29:42 +0700

    Update

---
+++ finalproject/app/src/main/java/hkr/finalproject/MainActivity.java
@@ -0,0 +1,27 @@
+package hkr.finalproject;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by HKR on 26/04/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+
+}
